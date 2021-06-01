465e5f8e662b9b4d8c8c658d51e8155105a144e3
helman101 <helmanandresortegon@gmail.com>
2021-06-01 11:21:29 +0200

    add 3 container component, 1 component and 1 reducer

---
+++ src/components/PokemonInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PokemonInfo = (props) => {
+  const { pokemon } = props;
+
+  return(
+    <div>
+      <div>{pokemon.game_indices.last.game_index}</div>
+      <img src={pokemon.sprites.front_default} />
+    </div>
+  );
+}
