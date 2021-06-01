8fda4b078456170cb657e1dfe74ba0aff984e37d
helman101 <helmanandresortegon@gmail.com>
2021-06-01 11:59:30 +0200

    add API folder with PokemonAPI.js file and create a module

---
+++ src/API/PokemonAPI.js
@@ -0,0 +1,21 @@
+const pokemonAPIModule = (() => {
+  let pokeAPI = async (url) => {
+    let result = await fetch(url);
+    let data = await result.json();
+    return data;
+  }
+
+  let searchByName = async (name) => {
+    let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    let data = await result.json();
+    return data;
+  }
+
+  let searchByType = async (type) => {
+    let result = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+    let data = await result.json();
+    return data;
+  }
+
+  return { pokeAPI, searchByName, searchByType }
+})
