ee9bcc7dc4bf505ba32a6cea24fe352d289ffaa4
helman101 <helmanandresortegon@gmail.com>
2021-06-03 16:17:05 +0200

    refactor

--- src/containers/PokemonPage.js
+++ src/containers/PokemonPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-const PokemonPage = () => (
-  <div>HI</div>
-);
+const PokemonPage = () => {
+  const data = useLocation();
+  console.log(data);
+  const { pokemon } = data;
+
+  return (
+    <div>{pokemon.name}</div>
+  );
+};
 
 export default PokemonPage;
