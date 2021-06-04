d274c420c2e59eaac8c5440b8aae2fab00d50c79
helman101 <helmanandresortegon@gmail.com>
2021-06-04 11:19:43 +0200

    refactor PokemonPages

--- src/containers/PokemonPage.js
+++ src/containers/PokemonPage.js
@@ -1,13 +1,58 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import styles from '../style.module.css';
 
-const PokemonPage = () => {
-  const data = useLocation();
-  const { pokemon } = data;
-
+const PokemonPage = (props) => {
+  const { pokemon } = props;
   return (
-    <div>{pokemon.name}</div>
+    <div>
+      <div>
+        <Link to="/">Back</Link>
+      </div>
+      <div>
+        <img
+          className={`${styles.pokePageSprite}`}
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+        />
+      </div>
+      <div>
+        <div>
+          <div>{pokemon.id}</div>
+          <div>{pokemon.name}</div>
+        </div>
+        <div>
+          <h2>Types</h2>
+          {pokemon.types.map((element) => (<div key={element.type.name}>{element.type.name}</div>))}
+        </div>
+        <div>
+          <h2>Abilities</h2>
+          {pokemon.abilities.map((element) => (
+            <div key={element.ability.name}>{element.ability.name}</div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2>Stats</h2>
+        {pokemon.stats.map((element) => (
+          <div key={element.stat.name}>
+            <span>{element.stat.name.concat(': ')}</span>
+            <span>{element.base_stat}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default PokemonPage;
+const mapStateToProps = (state) => ({
+  pokemon: state.currentPokemon,
+});
+
+PokemonPage.propTypes = {
+  pokemon: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps)(PokemonPage);
