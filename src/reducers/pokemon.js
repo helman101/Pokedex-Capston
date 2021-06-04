bc3cb9be282c083b2af9cdd3daa6e4c0e4a74619
helman101 <helmanandresortegon@gmail.com>
2021-06-04 10:01:12 +0200

    delete console logs

--- src/reducers/pokemon.js
+++ src/reducers/pokemon.js
@@ -4,13 +4,11 @@ const FILTER_CHANGE = 'FILTER_CHANGE';
 const pokemonReducer = (state = {}, action) => {
   switch (action.type) {
     case LOADED: {
-      console.log(action.payload);
       return action.payload;
     }
 
     case FILTER_CHANGE: {
       const newState = action.payload.pokemon.map((elem) => elem.pokemon);
-      console.log(newState);
       return {
         ...state,
         results: newState,
